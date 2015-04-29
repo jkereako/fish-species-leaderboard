d95300df929153e61325b9aa0c3ec21c947a4ab8
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-04-29 16:21:41 -0400

    Add: partial views
    Added partial views for flash messages and navigation. Also added
    internationalized strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def css_class_for(controller:)
+    'active' if params[:controller] == controller
+  end
+
+  def bootstrap_class_for(flash_type:)
+    case flash_type.to_sym
+      when :success
+        'alert-success'
+      when :error
+        'alert-danger'
+      when :alert
+        'alert-warning'
+      when :notice
+        'alert-info'
+      else
+        flash_type.to_s
+    end
+  end
 end
