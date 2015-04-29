6b6d041ad5b5d6a13545b044dc21e6918dcfe4a5
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-04-29 15:57:24 -0400

    Add: Devise and Devise Invitable files
    Added all of the files using generators. Edited `routes.rb` so no one can
    register an account.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # Prevent new users from registering new accounts. This is an invite-only app
+  devise_for :users, skip: 'registrations'
+
+  # Make the leaderboard the home page
   root 'leaderboard#index', as: 'leaderboard'
 end
