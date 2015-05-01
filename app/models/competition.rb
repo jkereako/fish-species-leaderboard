575f7a63fd88bd1820ee47d0476582335b7f530b
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-04-30 20:58:03 -0400

    Fix: bug in form
    The User fields for a Competition object now contain an indexed array where as
    before each input name was identical. This was fixed by adding the helper method
    `accepts_nested_attributes_for` to the Competition model and passing the symbol
    `:users` to `fields_for`

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,8 @@
 class Competition < ActiveRecord::Base
   has_many :users
   has_one :user, foreign_key: 'winner_id'
+
+  # Allows us to associate multiple User objects with 1 Competition object
+  # when updating
+  accepts_nested_attributes_for :users
 end
