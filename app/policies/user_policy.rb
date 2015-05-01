0a4be37bbe97327e6c1fc1d2b4fa833bdd254ea1
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-04-30 22:11:29 -0400

    Fix: check for nil
    Before checking if the user has the appropriate authorization, first check if
    the user is even signed in.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,8 +1,11 @@
 class UserPolicy < ApplicationPolicy
   def index?
+    return false if user.nil?
     user.role == 'administrator'
   end
+
   def show?
+    return false if user.nil?
     user.role == 'administrator'
   end
 end
