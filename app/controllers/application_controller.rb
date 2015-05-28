10e7261e2df4432033dfdb2ac3af6467dd0ea808
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-05-28 15:56:06 -0400

    Format: Catch views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   before_action :set_client_data
-  before_action :set_json_response, only: [:create, :update]
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
